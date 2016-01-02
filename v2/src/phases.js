98c6e82172998a50a39bb8d4efd8ffbb420cab84
geointerest <discoursemap@live.com>
2016-01-02 15:24:30 -0800

    Switched to v2
    Based on the JQuery and other tips from Baja's cousin

---
+++ v2/src/phases.js
@@ -0,0 +1,4 @@
+var phases =
+{type:"FeatureCollection",
+features:[
+{"type": "Feature", "properties": { "HRVST_CODE": 393 }, "geometry": { "type": "Polygon", "coordinates": [ [ [-55.22, 5.89], [-55.15, 5.89], [-55.15, 5.8], [-55.22, 5.8] ] ] } }]}
